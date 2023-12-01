628237d9ec1b8c1356d3419ba04ffecef96abc75
Tran Khoa <khoatranpc603@gmail.com>
2023-12-01 19:23:38 +0700

    add api

--- app.py
+++ app.py
@@ -74,4 +74,4 @@ def predicWithRandomForest(candidateId):
         predict = models.predicWithRandomForest(getData["dataTest"],getData["dataTrain"])
     return utils.getResponseJsonFormat(predict)
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=80, debug=True)
