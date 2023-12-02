d6da1f19293da27496bf7c7f68d78d677d748989
Tran Khoa <khoatranpc603@gmail.com>
2023-12-02 09:37:20 +0700

    remove cvv

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
 from flask import Flask, jsonify, request
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.models import load_model
 import numpy as np
 from flask_cors import CORS
 import json;
@@ -21,42 +18,6 @@ db=mongo.db
 def firstConnect():
     return 'Created By Trần Đăng Khoa'
 @app.route('/test-cnn')
-def tesCnn():
-    # Tạo dữ liệu mẫu dưới dạng JSON
-    json_data = '[{"expYear":1,"expTeach":1,"label":"Supporter"},{"expYear":2,"expTeach":1,"label":"Supporter"},{"expYear":1,"expTeach":2,"label":"Supporter"},{"expYear":2,"expTeach":2,"label":"Mentor"},{"expYear":1,"expTeach":3,"label":"Mentor"},{"expYear":2,"expTeach":3,"label":"Lecture"},{"expYear":3,"expTeach":1,"label":"Lecture"},{"expYear":3,"expTeach":2,"label":"Lecture"},{"expYear":3,"expTeach":3,"label":"Lecture"},{"expYear":4,"expTeach":1,"label":"Lecture"},{"expYear":4,"expTeach":2,"label":"Lecture"},{"expYear":4,"expTeach":3,"label":"Lecture"},{"expYear":5,"expTeach":1,"label":"Lecture"},{"expYear":5,"expTeach":2,"label":"Lecture"},{"expYear":5,"expTeach":3,"label":"Lecture"},{"expYear":6,"expTeach":1,"label":"Lecture"},{"expYear":6,"expTeach":2,"label":"Lecture"},{"expYear":6,"expTeach":3,"label":"Lecture"},{"expYear":7,"expTeach":1,"label":"Lecture"},{"expYear":7,"expTeach":2,"label":"Lecture"}]'
-    data = json.loads(json_data)
-
-    # Chuyển đổi dữ liệu JSON thành numpy array
-    features = np.array([[item["expYear"], item["expTeach"]] for item in data])
-    labels = np.array([1 if item["label"] == "Lecture" else (2 if item["label"] == "Mentor" else 3) for item in data])
-
-    # Tạo mô hình
-    model = Sequential()
-    model.add(Dense(32, input_dim=2, activation='relu'))
-    model.add(Dense(1, activation='softmax'))
-
-    # Biên soạn mô hình
-    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-
-    # Đào tạo mô hình
-    model.fit(features, labels, epochs=10)
-
-    # Lưu mô hình
-    model.save('my_model.h5')
-
-    # Load mô hình
-    loaded_model = load_model('my_model.h5')
-
-    # Dữ liệu mới cần dự đoán
-    new_json_data = '[{"expYear": 10, "expTeach": 10}]'
-    new_data = json.loads(new_json_data)
-    new_features = np.array([[item["expYear"], item["expTeach"]] for item in new_data])
-
-    # Thực hiện dự đoán
-    predictions = loaded_model.predict(new_features)
-    # data = utils.getJsonFormat(predictions)
-    print("Predictions:", predictions)
-    return str(predictions)
 
 @app.route('/random-forest')
 def accessRandomForest():
